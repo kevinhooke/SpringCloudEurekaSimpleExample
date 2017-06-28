30b957f84a30d4aba419bea16717d444ba8c8d76
Kevin Hooke <kevin.hooke@gmail.com>
2017-06-28 08:09:50 -0700

    initial commit for first service

---
+++ SpringBootService1/src/main/java/kh/springcloud/eureka/service1/Service1RestController.java
@@ -0,0 +1,18 @@
+package kh.springcloud.eureka.service1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kh.springcloud.eureka.domain.ExampleResponse;
+
+@RestController
+public class Service1RestController {
+
+	@GetMapping(value = "example1", produces = "application/json")
+	public ExampleResponse example1(){
+		ExampleResponse response = new ExampleResponse("example 1");
+		
+		return response;
+	}
+	
+}
---
+++ SpringBootService1/src/main/java/kh/springcloud/eureka/service1/SpringBootService1.java
@@ -0,0 +1,12 @@
+package kh.springcloud.eureka.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootService1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootService1.class, args);
+	}
+}
